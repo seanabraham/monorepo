92645c7177b1a1f785004f9b49c27538897e35f6
Sean Abraham <sean.abraham@airbnb.com>
2017-07-25 21:55:05 -0700

    Boolean false

--- generate_pipeline.rb
+++ generate_pipeline.rb
@@ -30,7 +30,7 @@ steps = []
 if run_android
   steps << {
     'trigger' => 'android-builds',
-    'async' => 'false',
+    'async' => false,
     'build' => {
       'commit' => ENV['BUILDKITE_COMMIT'],
       'message' => ENV['BUILDKITE_MESSAGE'],
@@ -42,7 +42,7 @@ end
 if run_ios
   steps << {
     'trigger' => 'ios-builds',
-    'async' => 'false',
+    'async' => false,
     'build' => {
       'commit' => ENV['BUILDKITE_COMMIT'],
       'message' => ENV['BUILDKITE_MESSAGE'],
@@ -54,7 +54,7 @@ end
 if run_other
   steps << {
     'trigger' => 'other-builds',
-    'async' => 'false',
+    'async' => false,
     'build' => {
       'commit' => ENV['BUILDKITE_COMMIT'],
       'message' => ENV['BUILDKITE_MESSAGE'],
