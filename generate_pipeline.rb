48682ee975dd249f5289a6ff12abf7997e1be795
Sean Abraham <sean.abraham@airbnb.com>
2017-07-25 21:46:09 -0700

    Remove async

--- generate_pipeline.rb
+++ generate_pipeline.rb
@@ -30,7 +30,6 @@ steps = []
 if run_android
   steps << {
     'trigger' => 'android-builds',
-    'async' => 'true',
     'build' => {
       'commit' => ENV['BUILDKITE_COMMIT'],
       'message' => ENV['BUILDKITE_MESSAGE'],
@@ -42,7 +41,6 @@ end
 if run_ios
   steps << {
     'trigger' => 'ios-builds',
-    'async' => 'true',
     'build' => {
       'commit' => ENV['BUILDKITE_COMMIT'],
       'message' => ENV['BUILDKITE_MESSAGE'],
@@ -54,7 +52,6 @@ end
 if run_other
   steps << {
     'trigger' => 'other-builds',
-    'async' => 'true',
     'build' => {
       'commit' => ENV['BUILDKITE_COMMIT'],
       'message' => ENV['BUILDKITE_MESSAGE'],
